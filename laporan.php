ebce2214f10ac75b47c5a4ee7381abdedb3a7730
ChuaJingHui <m-10388779@moe-dl.edu.my>
2026-02-11 22:28:41 +0800

    Enhance laporan.php with navigation and voting table
    Updated the laporan.php file to include navigation buttons and a table for displaying voting results.

---
+++ laporan.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Laporan</title>
+    </head>
+    <body>
+
+        <nav class="nav">
+        <div class="navTop">
+          <h1>SISTEM PENGUNDIAN PERMAINAN KEGEMARAN REMAJA</h1>
+        </div>
+        <div class="navButtons">
+          <a href="admin.php" class="nav-btn">Laporan</a>
+          <a href="kemaskini.php" class="nav-btn">Kemaskini</a>
+          <a href="login.php" class="nav-btn">Keluar</a>
+        </div>
+      </nav>
+
+      <table class="laporan">
+        <tr>
+          <td colspan="2"><h2>Jumlah Undian</h2></td>
+        </tr>
+        <tr>
+          <td>
+            <u>Permainan Tradisional</u>
+            <img src="https://umbiofficial.com/cdn/shop/articles/nostalgic-permainan-tradisional-you-might-miss-2509474.jpg?v=1757095395" alt="Permainan Tradisional" >
+            
+
+
+          </td>
+          <td>
+            <u>Permainan Digital</u>
+            <img src="https://www.sinarharian.com.my/sinarbestari/uploads/2021/06/1091985.jpg" alt="Permainan Digital" >
+            
+
+
+          </td>
+        </tr>
+      </table>
+
+        <footer>
+        <span class="copyright">Hak Cipta@Persatuan Penyelidikan Dan Kajian Remaja</span>
+      </footer>
+    </body>
+</html>
+
+
