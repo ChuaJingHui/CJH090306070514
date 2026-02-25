311d2f12460f042f1a406be86e998dea4c25066f
ChuaJingHui <m-10388779@moe-dl.edu.my>
2026-02-25 19:03:45 +0800

    Update laporan.php

--- laporan.php
+++ laporan.php
@@ -1,48 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
     <head>
-        <link rel="stylesheet" href="style.css">
-        <title>Laporan</title>
+        <title>Pengundian</title>
+        <link rel="stylesheet" href="styles.css">
+        
     </head>
     <body>
-
-        <nav class="nav">
+      <nav class="nav">
         <div class="navTop">
           <h1>SISTEM PENGUNDIAN PERMAINAN KEGEMARAN REMAJA</h1>
         </div>
         <div class="navButtons">
-          <a href="admin.php" class="nav-btn">Laporan</a>
-          <a href="kemaskini.php" class="nav-btn">Kemaskini</a>
-          <a href="login.php" class="nav-btn">Keluar</a>
+          <a href="laporan.php" class="nav-btn">LAPORAN</a>
+          <a href="kemaskini.php" class="nav-btn">KEMASKINI</a>
+          <a href="login.php" class="nav-btn">KELUAR</a>
         </div>
       </nav>
 
-      <table class="laporan">
-        <tr>
-          <td colspan="2"><h2>Jumlah Undian</h2></td>
-        </tr>
-        <tr>
-          <td>
-            <u>Permainan Tradisional</u>
-            <img src="https://umbiofficial.com/cdn/shop/articles/nostalgic-permainan-tradisional-you-might-miss-2509474.jpg?v=1757095395" alt="Permainan Tradisional" >
-            
-
-
-          </td>
-          <td>
-            <u>Permainan Digital</u>
-            <img src="https://www.sinarharian.com.my/sinarbestari/uploads/2021/06/1091985.jpg" alt="Permainan Digital" >
-            
-
-
-          </td>
-        </tr>
-      </table>
-
-        <footer>
+      <!--MEMBUAT PENGUNDIAN-->
+        <form onsubmit="" action="" method="POST">
+            <table class="undi">
+                <tr>
+                    <td colspan="2" style="padding: 5px;">
+                        <h3 style="margin: 0;" >Jumlah Undian</h3>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <u>Permainan Tradisional</u>
+                        <img src="https://umbiofficial.com/cdn/shop/articles/nostalgic-permainan-tradisional-you-might-miss-2509474.jpg?v=1757095395" alt="Permainan Tradisional" >
+                        <p><u>undian</u></p>
+                    </td>
+                    <td>
+                        <u>Permainan Digital</u>
+                        <img src="https://www.sinarharian.com.my/sinarbestari/uploads/2021/06/1091985.jpg" alt="Permainan Digital" >
+                        <p><u>undian</u></p>
+                    </td>
+                </tr>
+            <tr>
+                    <td colspan="2" style="text-align: center;">
+                        <input type="submit" value="CETAK" class="btn">
+                    </td>
+                </tr>
+            </table>
+        </form>
+      <footer>
         <span class="copyright">Hak Cipta@Persatuan Penyelidikan Dan Kajian Remaja</span>
       </footer>
     </body>
 </html>
-
-
